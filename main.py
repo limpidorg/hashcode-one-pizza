a9b5ac3ba68b9d007a7dc21c40985b7023e67eb5
EsprIx <3sprix.exe@gmail.com>
2022-02-18 11:41:58 +0100

    generated all answers

--- main.py
+++ main.py
@@ -7,68 +7,75 @@ import time
 
 from utils import getClients, getAllIngredientImpacts
 
-FILENAME = "C"
-FILE_PATH = f"./testFiles/{FILENAME}.txt"
+FILESNAMES = ["A", "B", "C", "D", "E"]
 
-startTime = time.time()
+for FILENAME in FILESNAMES:
+    FILE_PATH = f"./testFiles/{FILENAME}.txt"
 
-clients, ingredients, pClients = getClients(FILE_PATH)
-userscores = [0] * pClients
-ingredientsImpact = getAllIngredientImpacts(ingredients, clients, userscores)
+    startTime = time.time()
 
-clientSorted = []
-for client in clients:
-    meanImpact = 0
-    dislikeCount = 0
-    if client[1]:
-        for dislike in client[1]:
-            meanImpact += ingredientsImpact[dislike]
+    clients, ingredients, pClients = getClients(FILE_PATH)
+    userscores = [0] * pClients
+    ingredientsImpact = getAllIngredientImpacts(ingredients, clients, userscores)
+
+    clientSorted = []
+    for client in clients:
+        meanImpact = 0
+        dislikeCount = 0
+        if client[1]:
+            for dislike in client[1]:
+                meanImpact += ingredientsImpact[dislike]
+                dislikeCount += 1
+        else:
+            meanImpact += ingredientsImpact[""]
             dislikeCount += 1
-    else:
-        meanImpact += ingredientsImpact[""]
-        dislikeCount += 1
 
-    clientSorted.append([meanImpact / dislikeCount, client])
+        clientSorted.append([meanImpact / dislikeCount, client])
 
-clientSorted = sorted(clientSorted, key=itemgetter(0))
-clientSorted = list(reversed(clientSorted))
+    clientSorted = sorted(clientSorted, key=itemgetter(0))
+    clientSorted = list(reversed(clientSorted))
 
-approvedClients = []
-approvedLikes = []
-approvedDislikes = []
-for i in range(len(clientSorted)):
-    client = clientSorted[i][1]
-    clientDislikes = client[1]
-    clientLikes = client[0]
+    approvedClients = []
+    approvedLikes = []
+    approvedDislikes = []
+    for i in range(len(clientSorted)):
+        client = clientSorted[i][1]
+        clientDislikes = client[1]
+        clientLikes = client[0]
 
-    if (
-        not any(x in approvedLikes for x in clientDislikes)
-        and not any(x in approvedDislikes for x in clientLikes)
-        or clientDislikes == []
-    ):
-        approvedClients.append(clientSorted[i])
+        if (
+            not any(x in approvedLikes for x in clientDislikes)
+            and not any(x in approvedDislikes for x in clientLikes)
+            or clientDislikes == []
+        ):
+            approvedClients.append(clientSorted[i])
 
-        for ingredient in client[0]:
-            approvedLikes.append(ingredient)
-        for ingredient in client[1]:
-            approvedDislikes.append(ingredient)
+            for ingredient in client[0]:
+                approvedLikes.append(ingredient)
+            for ingredient in client[1]:
+                approvedDislikes.append(ingredient)
 
-res = []
-for i in approvedLikes:
-    if i not in res:
-        res.append(i)
+    res = []
+    for i in approvedLikes:
+        if i not in res:
+            res.append(i)
 
-print("\n")
-print(f"Ingredients count : {len(res)}")
-print(f"Ingredients list : {res}")
-print(f"Number of clients : {len(approvedClients)} / {len(clients)}")
+    print("\n")
+    print(f"Ingredients count : {len(res)}")
+    print(f"Ingredients list : {res}")
+    print(f"Number of clients : {len(approvedClients)} / {len(clients)}")
 
-with open(f"./answers/{FILENAME}.txt", "w") as f:
-    f.write(f"Ingredients count : {len(res)}\n")
-    f.write(f"Ingredients list : {res}\n")
-    f.write(f"Number of clients : {len(approvedClients)} / {len(clients)}")
+    with open(f"./answersStats/{FILENAME}.txt", "w") as f:
+        f.write(f"Ingredients count : {len(res)}\n")
+        f.write(f"Ingredients list : {res}\n")
+        f.write(f"Number of clients : {len(approvedClients)} / {len(clients)}")
 
+    print(f"\n Successfully written to ./answersStats/{FILENAME}.txt")
 
-print(f"\n Successfully written to ./answers/{FILENAME}.txt")
+    with open(f"./answers/{FILENAME}.txt", "w") as f:
+        resFormatted = " ".join(res)
+        f.write(f"{len(res)} {resFormatted}")
 
-print("\n--- Executed in %s seconds ---" % (time.time() - startTime))
+    print(f"\n Successfully written to ./answers/{FILENAME}.txt")
+
+    print("\n--- Executed in %s seconds ---" % (time.time() - startTime))
